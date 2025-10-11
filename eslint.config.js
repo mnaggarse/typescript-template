d1beec4e59baf97df323432c01a855b179fd7229
mnaggarse <mnaggarse@gmail.com>
2025-10-11 08:57:57 +0300

    First commit

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import perfectionist from "eslint-plugin-perfectionist";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["**/*.js"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  perfectionist.configs["recommended-natural"],
+);
