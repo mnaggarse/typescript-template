8307d8b7239ea9f7a8bcfbcad2744e7a4401d143
mnaggarse <mnaggarse@gmail.com>
2025-10-20 04:41:30 +0300

    Remove express

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import express from "express";
-
-const app = express();
-const port = process.env.PORT ?? "4000";
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+console.log("Hello, World!");
